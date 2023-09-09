f6c34fd701dc28b4c6435af52e1283d03c1b18cf
Selansis <wojtekmichalik24@gmail.com>
2023-09-09 12:16:17 +0200

    Fixing project to become more clear

---
+++ RestaurantAPI/WeatherForcastService.cs
@@ -0,0 +1,16 @@
+﻿namespace RestaurantAPI
+{
+    public class WeatherForcastService
+    {
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
