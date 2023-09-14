21de0916c0f403fc3ca31453d907093aee3e1ad5
Selansis <wojtekmichalik24@gmail.com>
2023-09-14 20:18:00 +0200

    zasilanie danymi, ale w microsoft server nie dziala

--- RestaurantAPI/Entities/Restaurant.cs
+++ RestaurantAPI/Entities/Restaurant.cs
@@ -5,8 +5,8 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public int Category { get; set; }
-        public bool HasDelievery { get; set; }
+        public string Category { get; set; }
+        public bool HasDelivery { get; set; }
         public string ContactEmail { get; set; }
         public string ContactNumber { get; set; }
 
--- RestaurantAPI/Program.cs
+++ RestaurantAPI/Program.cs
@@ -1,16 +1,27 @@
 using RestaurantAPI;
+using RestaurantAPI.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddTransient<IWeatherForcastService, WeatherForcastService>();
 builder.Services.AddControllers();
+builder.Services.AddDbContext<RestaurantDbContext>();
+builder.Services.AddScoped<RestaurantSeeder>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+
+var scope = app.Services.CreateScope();
+var seeder = scope.ServiceProvider.GetRequiredService<RestaurantSeeder>();
+seeder.Seed();
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
 
 app.MapControllers();
 
