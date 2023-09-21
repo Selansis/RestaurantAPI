6945c8ee3e1510f6a3ea67f2f01ab554dfa951eb
Selansis <wojtekmichalik24@gmail.com>
2023-09-21 17:36:46 +0200

    usunięcie kontrolera weatherforecast

--- RestaurantAPI/Program.cs
+++ RestaurantAPI/Program.cs
@@ -4,7 +4,6 @@ using RestaurantAPI.Entities;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddTransient<IWeatherForcastService, WeatherForcastService>();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<RestaurantDbContext>();
 builder.Services.AddScoped<RestaurantSeeder>();
