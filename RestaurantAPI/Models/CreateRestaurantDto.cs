3b9f7c0816db38ced612cc10cf29027e5de4115c
Selansis <wojtekmichalik24@gmail.com>
2023-10-10 18:43:08 +0200

    dodanie walidacji dla usera

--- RestaurantAPI/Models/CreateRestaurantDto.cs
+++ RestaurantAPI/Models/CreateRestaurantDto.cs
@@ -1,7 +1,11 @@
-﻿namespace RestaurantAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantAPI.Models
 {
     public class CreateRestaurantDto
     {
+        [Required]
+        [MaxLength(25)]
         public string? Name { get; set; }
         public string? Description { get; set; }
         public string? Category { get; set; }
@@ -9,7 +13,12 @@
         public string? ContactEmail { get; set; }
         public string? ContactNumber { get; set; }
 
+        [Required]
+        [MaxLength(50)]
         public string City { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string Street { get; set; }
         public string PostalCode { get; set; }
     }
